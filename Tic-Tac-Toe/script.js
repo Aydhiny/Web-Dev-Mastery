17f28cc972d706c91457046a94e2a39904fd963d
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2023-08-20 18:25:19 +0200

    tic tac toe prototype

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,50 @@
+let currentPlayer = 'X';
+const cells = document.querySelectorAll('.cell');
+
+function makeMove(cellIndex) {
+  const cell = cells[cellIndex];
+
+  if (cell.textContent === '') {
+    cell.textContent = currentPlayer;
+    cell.classList.add(currentPlayer);
+    
+    if (checkWin()) {
+      setTimeout(() => {
+        alert(currentPlayer + ' wins!');
+        resetGame();
+      }, 100);
+    } else if (isBoardFull()) {
+      setTimeout(() => {
+        alert('It\'s a draw!');
+        resetGame();
+      }, 100);
+    } else {
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  }
+}
+
+function checkWin() {
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  return winningCombos.some(combo => {
+    const [a, b, c] = combo;
+    return cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent;
+  });
+}
+
+function isBoardFull() {
+  return Array.from(cells).every(cell => cell.textContent !== '');
+}
+
+function resetGame() {
+  Array.from(cells).forEach(cell => {
+    cell.textContent = '';
+    cell.classList.remove('X', 'O');
+  });
+  currentPlayer = 'X';
+}
